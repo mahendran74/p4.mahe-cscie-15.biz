1c6bc72a21ee771d44280cdcbc4c45fc8082c41a
Mahendran Sreedevi <mahendran.nair@gmail.com>
2013-12-25 01:57:43 -0500

    PM Project view.

--- js/p4-pp.js
+++ js/p4-pp.js
@@ -9,6 +9,7 @@ function changeDateFormat(date_string) {
 
 /**
  * Process the Edit task button click
+ * 
  * @param vTaskID
  */
 function editTaskItem(task_id) {
@@ -21,20 +22,27 @@ function editTaskItem(task_id) {
       $('#addNewTaskWindow #task_task_desc').val(task.task_desc);
       $('#addNewTaskWindow #task_start_date').val(
           changeDateFormat(task.start_date));
-      $('#addNewTaskWindow #task_end_date').val(
-          changeDateFormat(task.end_date));
+      $('#addNewTaskWindow #task_end_date')
+          .val(changeDateFormat(task.end_date));
       if (task.status == "red") {
-        $('#addNewTaskWindow #task_status').simplecolorpicker('selectColor', '#dc2127');
+        $('#addNewTaskWindow #task_status').simplecolorpicker('selectColor',
+            '#dc2127');
       } else if (task.status == "yellow") {
-        $('#addNewTaskWindow #task_status').simplecolorpicker('selectColor', '#ffb878');
+        $('#addNewTaskWindow #task_status').simplecolorpicker('selectColor',
+            '#ffb878');
       } else {
-        $('#addNewTaskWindow #task_status').simplecolorpicker('selectColor', '#7bd148');
+        $('#addNewTaskWindow #task_status').simplecolorpicker('selectColor',
+            '#7bd148');
       }
-      $('#addNewTaskWindow #task_color').simplecolorpicker('selectColor', task.color);
-      $('#addNewTaskWindow #task_per_complete').slider('setValue', task.per_complete);
-      $('#addNewTaskWindow #task_assigned_to_id').select2('val', task.assigned_to_id);
+      $('#addNewTaskWindow #task_color').simplecolorpicker('selectColor',
+          task.color);
+      $('#addNewTaskWindow #task_per_complete').slider('setValue',
+          task.per_complete);
+      $('#addNewTaskWindow #task_assigned_to_id').select2('val',
+          task.assigned_to_id);
       $('#addNewTaskWindow #task_depends_on').select2('val', task.depends_on);
-      $('#addNewTaskWindow #task_groups_group_id').select2('val', task.groups_group_id);
+      $('#addNewTaskWindow #task_groups_group_id').select2('val',
+          task.groups_group_id);
       $('#alertAddNewTask').hide();
       $('#addTaskAction').hide();
       $('#editTaskAction').show();
@@ -45,45 +53,59 @@ function editTaskItem(task_id) {
 }
 /**
  * Process the Edit group button click
+ * 
  * @param vTaskID
  */
-function editGroupItem(vTaskID) {
-  var tempTaskItem = JSGantt.LookUpTask(vGanttChart, vTaskID);
-  if (tempTaskItem) {
-    $('#alertGroup').hide();
-    $('#groupName').val(tempTaskItem.getName);
-    $('#groupResource').val(tempTaskItem.getResource);
-    $('#groupParent').val(tempTaskItem.getParent);
-    $('#groupOpenClose').val(tempTaskItem.getOpen);
-    $('#groupID').val(tempTaskItem.getID);
-    $('#addGroupAction').hide();
-    $('#editGroupAction').show();
-    $('#groupModalLabel').text("Edit Group");
-    $('#newGroupWindow').modal('show');
-  }
+function editGroupItem(group_id) {
+  $.ajax({
+    type : 'post',
+    url : '/pm/get_group/' + group_id,
+    success : function(data) {
+      var group = jQuery.parseJSON(data);
+      $('#addNewGroupWindow #group_id').val(group.group_id);
+      $('#addNewGroupWindow #group_group_desc').val(group.group_desc);
+      $('#addNewGroupWindow #group_start_date').val(
+          changeDateFormat(group.start_date));
+      $('#addNewGroupWindow #group_end_date').val(
+          changeDateFormat(group.end_date));
+      $('#addNewGroupWindow #group_parent_group_id').select2('val',
+          group.parent_group_id);
+      $('#alertAddNewGroup').hide();
+      $('#addGroupAction').hide();
+      $('#editGroupAction').show();
+      $('#addNewGroupModalLabel').text("Edit Group");
+      $('#addNewGroupWindow').modal('show');
+    }
+  });
 }
 /**
  * Process the Edit milestone button click
+ * 
  * @param vTaskID
  */
-function editMilestone(vTaskID) {
-  var tempTaskItem = JSGantt.LookUpTask(vGanttChart, vTaskID);
-  $('#alertMilestone').hide();
-  $('#milestoneName').val(tempTaskItem.getName);
-  $('#milestoneDate').val(
-      (tempTaskItem.getStart().getMonth() + 1) + '/'
-          + tempTaskItem.getStart().getDate() + '/'
-          + tempTaskItem.getStart().getFullYear());
-  $('#milestoneResource').val(tempTaskItem.getResource);
-  $('#milestoneParent').val(tempTaskItem.getParent);
-  $('#msID').val(tempTaskItem.getID);
-  $('#addMsAction').hide();
-  $('#editMsAction').show();
-  $('#milestoneModalLabel').text("Edit Milestone");
-  $('#newMilestoneWindow').modal('show');
+function editMilestone(milestone_id) {
+  $.ajax({
+    type : 'post',
+    url : '/pm/get_milestone/' + milestone_id,
+    success : function(data) {
+      var milestone = jQuery.parseJSON(data);
+      $('#addNewMilestoneWindow #milestone_id').val(milestone.milestone_id);
+      $('#addNewMilestoneWindow #milestone_milestone_desc').val(
+          milestone.milestone_desc);
+      $('#addNewMilestoneWindow #milestone_milestone_date').val(
+          changeDateFormat(milestone.milestone_date));
+      $('#addNewMilestoneWindow #milestone_groups_group_id').select2('val',
+          milestone.groups_group_id);
+      $('#addNewMilestoneWindow #milestone_assigned_to_id').select2('val',
+          milestone.assigned_to_id);
+      $('#alertAddNewMilestone').hide();
+      $('#addMilestoneAction').hide();
+      $('#editMilestoneAction').show();
+      $('#addNewGroupModalLabel').text("Edit Milestone");
+      $('#addNewMilestoneWindow').modal('show');
+    }
+  });
 }
-
-
 
 $('#addNewUser').on('click', function(e) {
   e.preventDefault();
@@ -93,20 +115,48 @@ $('#addNewUser').on('click', function(e) {
 
 $('#addNewTask').on('click', function(e) {
   e.preventDefault();
+  $('#addNewTaskWindow #task_id').val('');
+  $('#addNewTaskWindow #task_task_desc').val('');
+  $('#addNewTaskWindow #task_start_date').val('');
+  $('#addNewTaskWindow #task_end_date').val('');
+  $('#addNewTaskWindow #task_status').simplecolorpicker('selectColor', '');
+  $('#addNewTaskWindow #task_color').simplecolorpicker('selectColor', '');
+  $('#addNewTaskWindow #task_per_complete').slider('setValue', '');
+  $('#addNewTaskWindow #task_assigned_to_id').select2('val', '');
+  $('#addNewTaskWindow #task_depends_on').select2('val', '');
+  $('#addNewTaskWindow #task_groups_group_id').select2('val', '');
   $('#alertAddNewTask').hide();
   $('#addTaskAction').show();
   $('#editTaskAction').hide();
+  $('#addNewTaskModalLabel').text("Add New Task");
   $('#addNewTaskWindow').modal('show');
 });
 
 $('#addNewGroup').on('click', function(e) {
   e.preventDefault();
+  $('#addNewGroupWindow #group_id').val('');
+  $('#addNewGroupWindow #group_group_desc').val('');
+  $('#addNewGroupWindow #group_start_date').val('');
+  $('#addNewGroupWindow #group_end_date').val('');
+  $('#addNewGroupWindow #task_parent_group_id').select2('val', '');
   $('#alertAddNewGroup').hide();
+  $('#addGroupAction').show();
+  $('#editGroupAction').hide();
+  $('#addNewGroupModalLabel').text("Add New Group");
   $('#addNewGroupWindow').modal('show');
 });
 
 $('#addNewMilestone').on('click', function(e) {
   e.preventDefault();
+  $('#addNewMilestoneWindow #milestone_id').val('');
+  $('#addNewMilestoneWindow #milestone_milestone_desc').val('');
+  $('#addNewMilestoneWindow #milestone_milestone_date').val('');
+  $('#addNewMilestoneWindow #milestone_groups_group_id').select2('val', '');
+  $('#addNewMilestoneWindow #milestone_assigned_to_id').select2('val', '');
+  $('#alertAddNewMilestone').hide();
+  $('#addMilestoneAction').show();
+  $('#editMilestoneAction').hide();
+  $('#addNewMilestoneModalLabel').text("Add New Milestone");
   $('#alertAddNewMilestone').hide();
   $('#addNewMilestoneWindow').modal('show');
 });
@@ -127,7 +177,7 @@ $('#milestone_per_complete').slider({
   }
 });
 
-// Select 
+// Select
 $("#task_assigned_to_id").select2({
   data : {
     results : resList
@@ -188,42 +238,242 @@ $('#task_color').simplecolorpicker({
   theme : 'glyphicons'
 });
 
-//New user submit
-$("#addNewUserForm").validate(
-    {
-      showErrors : function(errorMap, errorList) {
+// New user submit
+$("#addNewUserForm").validate({
+  showErrors : function(errorMap, errorList) {
 
-        // Clean up any tooltips for valid elements
-        $.each(this.validElements(), function(index, element) {
-          var $element = $(element);
-          $element.removeClass("has-error").addClass("has-success");
-          $element.data("title", "").tooltip("destroy");
-        });
+    // Clean up any tooltips for valid elements
+    $.each(this.validElements(), function(index, element) {
+      var $element = $(element);
+      $element.removeClass("has-error").addClass("has-success");
+      $element.data("title", "").tooltip("destroy");
+    });
 
-        // Create new tooltips for invalid elements
-        $.each(errorList, function(index, error) {
-          var $element = $(error.element);
-          $element.removeClass("has-success").addClass("has-error");
-          $element.tooltip("destroy").data("title", error.message).tooltip({
-            'placement' : 'bottom'
-          });
-        });
-      },
+    // Create new tooltips for invalid elements
+    $.each(errorList, function(index, error) {
+      var $element = $(error.element);
+      $element.removeClass("has-success").addClass("has-error");
+      $element.tooltip("destroy").data("title", error.message).tooltip({
+        'placement' : 'bottom'
+      });
+    });
+  },
 
-      submitHandler : function(form) {
-        var formData = $(form).serialize();
-        var project_id = $("#project_id").val();
-        $.ajax({
-          type : 'post',
-          url : '/users/p_adduser',
-          data : formData,
-          success : function(data) {
-            console.log(data);
-            bootbox.alert(data, function(result) {
-              window.location = "/pm/project_details/"+project_id;
-            });
-          }
+  submitHandler : function(form) {
+    var formData = $(form).serialize();
+    var project_id = $("#project_id").val();
+    $.ajax({
+      type : 'post',
+      url : '/users/p_adduser',
+      data : formData,
+      success : function(data) {
+        console.log(data);
+        bootbox.alert(data, function(result) {
+          window.location = "/pm/project_details/" + project_id;
         });
-        return false;
       }
+    });
+    return false;
+  }
+});
+
+// New task submit
+$("#addNewTaskForm").validate({
+  showErrors : function(errorMap, errorList) {
+
+    // Clean up any tooltips for valid elements
+    $.each(this.validElements(), function(index, element) {
+      var $element = $(element);
+      $element.removeClass("has-error").addClass("has-success");
+      $element.data("title", "").tooltip("destroy");
+    });
+
+    // Create new tooltips for invalid elements
+    $.each(errorList, function(index, error) {
+      var $element = $(error.element);
+      $element.removeClass("has-success").addClass("has-error");
+      $element.tooltip("destroy").data("title", error.message).tooltip({
+        'placement' : 'bottom'
+      });
+    });
+  },
+
+  submitHandler : function(form) {
+    var formData = $(form).serialize();
+    var project_id = $('#project_id').val();
+    var submit_url = '/pm/p_add_task';
+    if ($('#task_id').val() != "") {
+      submit_url = '/pm/p_update_task';
+    }
+    $.ajax({
+      type : 'post',
+      url : submit_url,
+      data : formData,
+      success : function(data) {
+        console.log(data);
+        bootbox.alert(data, function(result) {
+          window.location = "/pm/project_details/" + project_id;
+        });
+      }
+    });
+    return false;
+  }
+});
+
+// New group submit
+$("#addNewGroupForm").validate({
+  showErrors : function(errorMap, errorList) {
+
+    // Clean up any tooltips for valid elements
+    $.each(this.validElements(), function(index, element) {
+      var $element = $(element);
+      $element.removeClass("has-error").addClass("has-success");
+      $element.data("title", "").tooltip("destroy");
+    });
+
+    // Create new tooltips for invalid elements
+    $.each(errorList, function(index, error) {
+      var $element = $(error.element);
+      $element.removeClass("has-success").addClass("has-error");
+      $element.tooltip("destroy").data("title", error.message).tooltip({
+        'placement' : 'bottom'
+      });
+    });
+  },
+
+  submitHandler : function(form) {
+    var formData = $(form).serialize();
+    var project_id = $('#project_id').val();
+    var submit_url = '/pm/p_add_group';
+    if ($('#group_id').val() != "") {
+      submit_url = '/pm/p_update_group';
+    }
+    $.ajax({
+      type : 'post',
+      url : submit_url,
+      data : formData,
+      success : function(data) {
+        console.log(data);
+        bootbox.alert(data, function(result) {
+          window.location = "/pm/project_details/" + project_id;
+        });
+      }
+    });
+    return false;
+  }
+});
+
+// New milestone submit
+$("#addNewMilestoneForm").validate({
+  showErrors : function(errorMap, errorList) {
+
+    // Clean up any tooltips for valid elements
+    $.each(this.validElements(), function(index, element) {
+      var $element = $(element);
+      $element.removeClass("has-error").addClass("has-success");
+      $element.data("title", "").tooltip("destroy");
+    });
+
+    // Create new tooltips for invalid elements
+    $.each(errorList, function(index, error) {
+      var $element = $(error.element);
+      $element.removeClass("has-success").addClass("has-error");
+      $element.tooltip("destroy").data("title", error.message).tooltip({
+        'placement' : 'bottom'
+      });
+    });
+  },
+
+  submitHandler : function(form) {
+    var formData = $(form).serialize();
+    var project_id = $('#project_id').val();
+    var submit_url = '/pm/p_add_milestone';
+    if ($('#milestone_id').val() != "") {
+      submit_url = '/pm/p_update_milestone';
+    }
+    $.ajax({
+      type : 'post',
+      url : submit_url,
+      data : formData,
+      success : function(data) {
+        console.log(data);
+        bootbox.alert(data, function(result) {
+          window.location = "/pm/project_details/" + project_id;
+        });
+      }
+    });
+    return false;
+  }
+});
+
+// Delete task
+$('#taskDelButton').on(
+    'click',
+    function(e) {
+      e.preventDefault();
+      var task_id = $('#task_id').val();
+      var project_id = $('#project_id').val();
+      bootbox.confirm("Are you sure you want to delete this task ?", function(
+          result) {
+        if (result) {
+          $.ajax({
+            type : 'post',
+            url : '/pm/delete_task/' + task_id,
+            success : function(data) {
+              console.log(data);
+              bootbox.alert(data, function(result) {
+                window.location = "/pm/project_details/" + project_id;
+              });
+            }
+          });
+        }
+      });
+    });
+
+//Delete group
+$('#groupDelButton').on(
+    'click',
+    function(e) {
+      e.preventDefault();
+      var group_id = $('#group_id').val();
+      var project_id = $('#project_id').val();
+      bootbox.confirm("Are you sure you want to delete this group ?", function(
+          result) {
+        if (result) {
+          $.ajax({
+            type : 'post',
+            url : '/pm/delete_group/' + group_id,
+            success : function(data) {
+              console.log(data);
+              bootbox.alert(data, function(result) {
+                window.location = "/pm/project_details/" + project_id;
+              });
+            }
+          });
+        }
+      });
+    });
+
+//Delete milestone
+$('#milestoneDelButton').on(
+    'click',
+    function(e) {
+      e.preventDefault();
+      var milestone_id = $('#milestone_id').val();
+      var project_id = $('#project_id').val();
+      bootbox.confirm("Are you sure you want to delete this milestone ?", function(
+          result) {
+        if (result) {
+          $.ajax({
+            type : 'post',
+            url : '/pm/delete_milestone/' + milestone_id,
+            success : function(data) {
+              console.log(data);
+              bootbox.alert(data, function(result) {
+                window.location = "/pm/project_details/" + project_id;
+              });
+            }
+          });
+        }
+      });
     });
